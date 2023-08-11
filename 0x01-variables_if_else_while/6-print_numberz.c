e3fc9bf8c34f149d8099356faf8f98a8b5c9f09b
Awab Omer <awab228@yahoo.com>
2023-08-11 15:16:36 -0700

    adds the task number 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,16 +3,18 @@
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
  * followed by a new line
- *
+ * we init n to 48 which is 0 in asci, because putchar supports chars only
+ * and this is the shortest way.
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
+	{
 		putchar(n);
-
+	}
 	putchar('\n');
 
 	return (0);
