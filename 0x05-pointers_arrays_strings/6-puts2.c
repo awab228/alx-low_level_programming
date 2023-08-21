a7249164d3c93e79a7f38b366af55c53d7d5739d
Awab Omer <awab228@yahoo.com>
2023-08-21 16:26:56 -0700

    adds files for task 7

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: the string to print the chars from
+ *
+  */
+void puts2(char *str)
+{
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
+}
