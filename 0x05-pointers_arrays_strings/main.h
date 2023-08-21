b31c7c06c373f0e4ddaf269d108507b822917938
Awab Omer <awab228@yahoo.com>
2023-08-21 15:57:39 -0700

    fix for custom_putchar function

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+1;95;0c#define MAIN_H
 
 #include <stdio.h>
 
@@ -7,5 +7,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c);
 
 #endif /* MAIN_H */
