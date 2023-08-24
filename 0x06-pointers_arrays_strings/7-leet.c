86a8ed130815f1872708fe13c3d2f86942cec356
Awab Omer <awab228@yahoo.com>
2023-08-24 15:28:03 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to be encoded
+ *
+ * Return: the final string;
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
