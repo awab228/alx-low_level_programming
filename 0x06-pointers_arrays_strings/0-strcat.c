b01c108e0552c9c3d32fd4cc7d88e1ff3855414b
Awab Omer <awab228@yahoo.com>
2023-08-24 13:42:37 -0700

    adds files for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the string to append to
+ * @src: the string to appended
+ *
+ * Return: the pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
