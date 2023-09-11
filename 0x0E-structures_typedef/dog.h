a0bf4c003772471a727552ff96e6fc06bccd1b15
Awab Omer <awab228@yahoo.com>
2023-09-11 16:36:38 -0700

    task 0 poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
