d132bc418c2912271850ab26983bd9a42af620a5
Awab Omer <awab228@yahoo.com>
2023-09-13 17:09:21 -0700

    task 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_opcodes - prints the opcodes of a function
+ * @func: pointer to the function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(void *func, int n)
+{
+    unsigned char *opcode = (unsigned char *)func;
+
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%.2hhx", opcode[i]);
+        if (i < n - 1)
+            printf(" ");
+    }
+    printf("\n");
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <number of opcodes>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    if (n < 0)
+    {
+        fprintf(stderr, "Error: Number of opcodes must be non-negative\n");
+        return 2;
+    }
+
+    print_opcodes((void *)&main, n);
+    return 0;
+}
