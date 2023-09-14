59491b92cc60fd3b67b1f7c60c4cefafdf8d1f57
Awab Omer <awab228@yahoo.com>
2023-09-13 17:24:52 -0700

    fix error requiremnt

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s <number of opcodes>\n", argv[0]);
+		fprintf(stderr, "Error");
 		return (1);
 	}
 
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
 
 	if (n < 0)
 	{
-		fprintf(stderr, "Error: Number of opcodes must be non-negative\n");
+		fprintf(stderr, "Error");
 		return (2);
 	}
 
