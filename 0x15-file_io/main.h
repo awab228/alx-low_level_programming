161e1225cee11d3659444df0b18fa8232c60246b
awab228 <awab228@yahoo.com>
2023-10-03 02:00:25 +0200

    task 100

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+/* advanced task */
+#include <elf.h>
+
+#define __home inline
+#define __silent __attribute__((unused))
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
